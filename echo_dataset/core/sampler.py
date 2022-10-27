da175cbbe66ac0f2b49d0effc1fd8f7aa81ca254
Andrei Margert <andrei.margert@wur.nl>
2022-10-27 15:56:53 +0200

    start on sampler

--- echo_dataset/core/sampler.py
+++ echo_dataset/core/sampler.py
@@ -1,19 +1,31 @@
 import xarray as xr
 
+from typing import Sequence
+
 
 __all__ = ["ISampler"]
 
 
 class ISampler:
+    _window_size: tuple[int, int]
+
     def __call__(self, *args, **kwargs) -> dict[str, any]:
         raise NotImplementedError
 
+    @property
+    def window_size(self) -> tuple[int, int]:
+        return self._window_size
+
 
 class RandomSchoolSampler(ISampler):
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, window_size: Sequence[int]) -> None:
+        if len(window_size) == 2 and all(isinstance(v, int) for v in window_size):
+            self._window_size = tuple(window_size)
+        else:
+            raise TypeError
 
     def __call__(self, *args, **kwargs) -> dict[str, xr.Dataset]:
+
         raise NotImplementedError
 
     def _generate_point(self) -> list[int, int]:

75291f1bd1f50bd985cf3e4174546c93b3ae287d
Andre1M <andrei95@gmail.com>
2023-01-11 11:34:30 +0100

    Adds Random Background Sampler.

--- echo_dataset/core/sampler.py
+++ echo_dataset/core/sampler.py
@@ -4,7 +4,7 @@ from typing import Sequence
 import random
 
 
-__all__ = ["ISampler", "RandomSchoolSampler"]
+__all__ = ["ISampler", "RandomSchoolSampler", "RandomBackgroundSampler"]
 
 
 class ISampler:
@@ -61,12 +61,44 @@ class RandomSchoolSampler(ISampler):
             )
 
 
-class RandomBottomSampler(ISampler):
-    def __init__(self) -> None:
-        raise NotImplementedError
+class RandomBackgroundSampler(ISampler):
+    def __init__(self, window_size: Sequence[int]) -> None:
+        if len(window_size) == 2 and all(
+            isinstance(v, int) for v in window_size
+        ):
+            # noinspection PyTypeChecker
+            self._window_size = tuple(window_size)
+        else:
+            raise Exception("Must be a sequence of exactly two integers")
 
-    def __call__(self, *args, **kwargs) -> dict[str, xr.Dataset]:
-        raise NotImplementedError
+    def __call__(self, ds: "IEchoDataset") -> dict[str, xr.Dataset]:
+        if self._categories is None:
+            raise ValueError
+        x_max = ds.total_num_pings - self._window_size[0] - 1
+        while True:
+            while True:
+                x = random.randint(0, x_max)
+                max_ping = None
+                for i, k in enumerate(ds.ping_range_map):
+                    if x < k:
+                        max_ping = k
+                        break
+                assert i is not None
+                assert max_ping is not None
+                if (x + self._window_size[0]) < max_ping:
+                    break
+            y = random.randint(0, ds.ping_range_map[max_ping] - self.window_size[1] - 1)
+            window = ds.crop(
+                cruise_idx=i,
+                box=[
+                    x,
+                    y,
+                    x + self._window_size[0],
+                    y + self._window_size[1],
+                ],
+            )
+            if not window["annotations"]["annotation"].any().compute():
+                return window
 
     def _generate_point(self) -> list[int, int]:
         raise NotImplementedError

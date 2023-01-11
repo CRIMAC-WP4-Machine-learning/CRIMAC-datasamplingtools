31848b13c3430918e59bce9cc9fc12ebdaac6fb7
Andre1M <andrei95@gmail.com>
2023-01-11 11:38:44 +0100

    Adds a simple user interface.

---
+++ echo_dataset/eds/__init__.py
@@ -0,0 +1,6 @@
+from echo_dataset.core.dataset import EchoDataset
+from echo_dataset.core.compound_sampler import CompoundSampler
+from echo_dataset.core.sampler import (
+    RandomSchoolSampler,
+    RandomBackgroundSampler,
+)

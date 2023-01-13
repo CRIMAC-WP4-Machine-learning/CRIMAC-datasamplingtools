8af366282c17a3c320bcb244edaebeaac0527f39
Andrei Margert <andrei.margert@wur.nl>
2023-01-13 11:59:38 +0100

    tidy up

--- echo_dataset/eds/__init__.py
+++ echo_dataset/eds/__init__.py
@@ -1,6 +1,1 @@
-from echo_dataset.core.dataset import EchoDataset
-from echo_dataset.core.compound_sampler import CompoundSampler
-from echo_dataset.core.sampler import (
-    RandomSchoolSampler,
-    RandomBackgroundSampler,
-)
+from echo_dataset.core import *

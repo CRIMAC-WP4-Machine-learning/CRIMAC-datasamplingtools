bc5b992ec621c75b68ba46d3964414a3894a4759
Andrei Margert <andrei.margert@wur.nl>
2023-03-20 17:27:14 +0100

    adds dataset helper functions to utils

---
+++ data_sampling_tools/utils/dataset.py
@@ -0,0 +1,23 @@
+from ..core import IStatelessSampler, IStatefulSampler, ICruiseList
+
+from typing import Union, Optional
+
+
+def eval_dataset_length(
+    sampler: Union[IStatelessSampler, IStatefulSampler],
+    pseudo_length: Optional[int] = None,
+) -> int:
+    if issubclass(type(sampler), IStatelessSampler):
+        length = int(pseudo_length) if pseudo_length is not None else 1
+    elif issubclass(type(sampler), IStatefulSampler):
+        length = len(sampler)
+    else:
+        raise TypeError("Wrong sampler type")
+    return length
+
+
+def filter_cruise_list(cfg: dict[str, any]) -> ICruiseList:
+    pass
+
+
+__all__ = ["eval_dataset_length", "filter_cruise_list"]
